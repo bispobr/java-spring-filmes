52ee3d632f8a5d77692bca0422a610bed4d1ffcf
bispobr <156684382+bispobr@users.noreply.github.com>
2025-07-06 10:27:19 -0300

    feat: add service filme

---
+++ src/main/java/com/example/filme/service/FilmeService.java
@@ -0,0 +1,42 @@
+package com.example.filme.service;
+
+import com.example.filme.model.Filme;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class FilmeService {
+
+    private final WebClient webClient;
+
+    public FilmeService (WebClient.Builder builder){
+        log.info("Acessando Base Principal");
+        this.webClient = builder.baseUrl("https://data.sfgov.org/resource/yitu-d5am.json").build();
+    }
+
+    public List<Filme> pegarTodosFilmes(){
+        log.info("Portando filmes");
+        return  webClient.get().retrieve().bodyToFlux(Filme.class).collectList().block();
+    }
+
+    public  List<Filme> FiltrobyTitulo(String busca){
+        log.info("filtro por titulo");
+        return  pegarTodosFilmes().stream().filter(f ->f.getTitle() != null && f.getTitle().toLowerCase().contains(busca.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public  List<String> prefixo(String prefixo){
+        log.info("filtro por prefixo");
+        return pegarTodosFilmes().stream().map(Filme::getTitle).filter(Objects::nonNull).filter(t->t.toLowerCase().startsWith(prefixo.toLowerCase())).distinct().sorted().limit(10).collect(Collectors.toList());
+    }
+
+    public  List<Filme> FiltrobyDiretor(String busca){
+        log.info("filtro por diretor");
+        return  pegarTodosFilmes().stream().filter(f ->f.getDirector() != null && f.getDirector().toLowerCase().contains(busca.toLowerCase())).collect(Collectors.toList());
+    }
+}
