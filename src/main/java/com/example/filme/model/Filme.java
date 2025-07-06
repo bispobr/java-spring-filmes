22109732a14197c2870392c566c810400d15f82f
bispobr <156684382+bispobr@users.noreply.github.com>
2025-07-06 10:26:44 -0300

    feat: add entity filme

---
+++ src/main/java/com/example/filme/model/Filme.java
@@ -0,0 +1,19 @@
+package com.example.filme.model;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+public class Filme {
+    private String title;
+    private String release_year;
+    private String locations;
+    private String production_company;
+    private String distributor;
+    private String director;
+    private String actor_1;
+    private String actor_2;
+    private String actor_3;
+    private String longitude;
+    private String latitude;
+}
