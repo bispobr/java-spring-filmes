0829abb3b1df709379d074322d317ef522300271
bispobr <156684382+bispobr@users.noreply.github.com>
2025-12-21 12:35:30 -0300

    refactor: refatoração do codigo
    refatoração do código , adição de novos endpoint e uso do mapper

--- src/main/java/com/example/filme/controller/FilmeController.java
+++ src/main/java/com/example/filme/controller/FilmeController.java
@@ -1,18 +1,22 @@
 package com.example.filme.controller;
 
+import com.example.filme.dto.BuscaRequisicaoDTO;
+import com.example.filme.dto.BuscaRespostaDTO;
+import com.example.filme.dto.FilmeRespostaDTO;
+import com.example.filme.mapper.FilmeMapper;
 import com.example.filme.model.Filme;
 import com.example.filme.service.FilmeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Slf4j
 @RestController
@@ -21,30 +25,64 @@ public class FilmeController {
     @Autowired
     private FilmeService filmeService;
 
-    @GetMapping
-    @Operation(description = "Endpoint responsável por listar dados do filme")
+    @Autowired
+    FilmeMapper mapper;
+
+    @GetMapping("/TodosFilmes")
+    @Operation(description = "Endpoint responsável por listar Todos os filmes do acervo")
     @ApiResponse(responseCode = "200", description = "Listagem bem sucedida")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public List<Filme> listarfilmes(@RequestParam Optional<String> titulo){
-        log.info("Requisição de listagem recebida");
-        return titulo.map(filmeService::FiltrobyTitulo).orElseGet(filmeService::pegarTodosFilmes);
+    public ResponseEntity<List<FilmeRespostaDTO>> listarfilmes(){
+        log.info("Requisição para Listar todos os filmes recebida");
+        return ResponseEntity.ok().body(mapper.paraRespostaList(filmeService.pegarTodosFilmes()));
     }
 
-    @GetMapping ("/prefixo")
-    @Operation(description = "Endpoint responsável por listar dados do filme atraves do prefixo")
+    @PostMapping("/titulo")
+    @Operation(description = "Endpoint responsável por listar filmes por titulo e seus dados presente no acervo")
     @ApiResponse(responseCode = "200", description = "Listagem bem sucedida")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
+    @ApiResponse(responseCode = "404", description = "Não encontrado")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public List<String> autocompletar(@RequestParam("prefixo") String prefixo ){
-        log.info("Requisição de listagem recebida");
-        return filmeService.prefixo(prefixo);
+    public ResponseEntity<Object> listarfilmesTitulo(@RequestBody @Valid BuscaRequisicaoDTO dto){
+
+        log.info("Requisição para listar Filmes por titulo  recebida");
+        List<Filme> encontrado = filmeService.FiltrobyTitulo(dto);
+        if (encontrado.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(mapper.paraRespostaList(encontrado)) ;
+
     }
 
-    @GetMapping("/diretor")
+    @PostMapping("/buscaprefixo")
+    @Operation(description = "Endpoint responsável por listar titulos de filmes do acervo baseado em um prefixo")
+    @ApiResponse(responseCode = "200", description = "Listagem bem sucedida")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
+    @ApiResponse(responseCode = "404", description = "Não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro interno")
+    public ResponseEntity<List<BuscaRespostaDTO>> prefixo(@RequestBody @Valid BuscaRequisicaoDTO prefixo ){
+        log.info("Requisição de busca por titulo  recebida");
+        List<String> encontrado = filmeService.prefixo(prefixo);
+        if (encontrado.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(mapper.paraRespostaBuscaList(encontrado));
+    }
+
+    @PostMapping("/diretor")
     @Operation(description = "Endpoint responsável por listar filme feitos pelo diretor")
     @ApiResponse(responseCode = "200", description = "Listagem bem sucedida")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
+    @ApiResponse(responseCode = "404", description = "Não encontrado")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public List<Filme> listarfilmesDiretor(@RequestParam Optional<String> diretor){
+    public ResponseEntity<List<FilmeRespostaDTO>> listarfilmesDiretor(@RequestBody @Valid BuscaRequisicaoDTO diretor){
         log.info("Requisição de listagem recebida");
-        return diretor.map(filmeService::FiltrobyDiretor).orElseGet(filmeService::pegarTodosFilmes);
+        List<Filme> encontardo = filmeService.FiltrobyDiretor(diretor);
+
+        if (encontardo.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(mapper.paraRespostaList(encontardo));
     }
 }
