b4cb577a9a608c73d8be6a2f476fc582a2cb6faa
bispobr <156684382+bispobr@users.noreply.github.com>
2025-12-21 12:08:46 -0300

    feat: add cache e uso de logs

--- src/main/java/com/example/filme/service/FilmeService.java
+++ src/main/java/com/example/filme/service/FilmeService.java
@@ -1,7 +1,9 @@
 package com.example.filme.service;
 
+import com.example.filme.dto.BuscaRequisicaoDTO;
 import com.example.filme.model.Filme;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,23 +22,24 @@ public class FilmeService {
         this.webClient = builder.baseUrl("https://data.sfgov.org/resource/yitu-d5am.json").build();
     }
 
+    @Cacheable(value = "dados")
     public List<Filme> pegarTodosFilmes(){
-        log.info("Portando filmes");
+        log.info("Listagem concluida");
         return  webClient.get().retrieve().bodyToFlux(Filme.class).collectList().block();
     }
 
-    public  List<Filme> FiltrobyTitulo(String busca){
+    public  List<Filme> FiltrobyTitulo(BuscaRequisicaoDTO busca){
         log.info("filtro por titulo");
-        return  pegarTodosFilmes().stream().filter(f ->f.getTitle() != null && f.getTitle().toLowerCase().contains(busca.toLowerCase())).collect(Collectors.toList());
+        return  pegarTodosFilmes().stream().filter(f ->f.getTitle() != null && f.getTitle().toLowerCase().contains(busca.busca().toLowerCase())).collect(Collectors.toList());
     }
 
-    public  List<String> prefixo(String prefixo){
+    public  List<String> prefixo(BuscaRequisicaoDTO prefixo){
         log.info("filtro por prefixo");
-        return pegarTodosFilmes().stream().map(Filme::getTitle).filter(Objects::nonNull).filter(t->t.toLowerCase().startsWith(prefixo.toLowerCase())).distinct().sorted().limit(10).collect(Collectors.toList());
+        return pegarTodosFilmes().stream().map(Filme::getTitle).filter(Objects::nonNull).filter(t->t.toLowerCase().startsWith(prefixo.busca().toLowerCase())).distinct().sorted().limit(10).collect(Collectors.toList());
     }
 
-    public  List<Filme> FiltrobyDiretor(String busca){
+    public  List<Filme> FiltrobyDiretor(BuscaRequisicaoDTO busca){
         log.info("filtro por diretor");
-        return  pegarTodosFilmes().stream().filter(f ->f.getDirector() != null && f.getDirector().toLowerCase().contains(busca.toLowerCase())).collect(Collectors.toList());
+        return  pegarTodosFilmes().stream().filter(f ->f.getDirector() != null && f.getDirector().toLowerCase().contains(busca.busca().toLowerCase())).collect(Collectors.toList());
     }
 }
