d03f35b1e988fdcf2752d79eb28b4b57b59eb837
bispobr <156684382+bispobr@users.noreply.github.com>
2025-12-21 10:56:50 -0300

    feat: add DTOs de reposta

---
+++ src/main/java/com/example/filme/dto/BuscaRespostaDTO.java
@@ -0,0 +1,4 @@
+package com.example.filme.dto;
+
+public record BuscaRespostaDTO(String Resultado) {
+}
---
+++ src/main/java/com/example/filme/dto/FilmeRespostaDTO.java
@@ -0,0 +1,6 @@
+package com.example.filme.dto;
+
+public record FilmeRespostaDTO(String title, String release_year, String locations, String production_company,
+                               String distributor, String director, String actor_1, String actor_2, String actor_3, String longitude,
+                               String latitude) {
+}
