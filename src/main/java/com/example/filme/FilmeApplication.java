1d368208635d5e6275db712acb542cd61ed67603
bispobr <156684382+bispobr@users.noreply.github.com>
2025-12-21 12:09:18 -0300

    feat: hebilitar cache na aplicação

--- src/main/java/com/example/filme/FilmeApplication.java
+++ src/main/java/com/example/filme/FilmeApplication.java
@@ -2,8 +2,10 @@ package com.example.filme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FilmeApplication {
 
 	public static void main(String[] args) {
